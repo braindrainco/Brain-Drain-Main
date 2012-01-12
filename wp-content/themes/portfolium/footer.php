9defb2821eefc8cbada2ee430b4f3bff81d73c11
Matthew Strom <matthew.h.strom@gmail.com>
2012-01-12 14:45:40 -0600

    let's try to clone again

---
+++ wp-content/themes/portfolium/footer.php
@@ -0,0 +1,15 @@
+            </div>
+        </div>
+        <div class="footer clear">
+            <div class="footer_inn">
+                <div class="left">
+                    <p>&copy; 2012 <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a></p>
+                </div>
+                <div class="right">
+                    <p>Designed by Brain Drain</p>
+                </div>
+            </div>
+        </div>
+        <!-- Page generated: <?php timer_stop(1); ?> s, <?php echo get_num_queries(); ?> queries -->
+	</body>
+</html>
